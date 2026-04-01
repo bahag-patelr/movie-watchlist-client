caba475aa508da8130f9bae37b9e1991e2d846aa
Maximilian Reed <maximilian.reed@bahag.com>
2026-04-01 11:44:57 +0200

    wrapped app in browser router

--- my-app/src/main.jsx
+++ my-app/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import AppRouter from "./AppRouter.jsx";
+import {BrowserRouter} from "react-router";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <AppRouter />
+      <BrowserRouter>
+          <AppRouter />
+      </BrowserRouter>
   </StrictMode>,
 )
